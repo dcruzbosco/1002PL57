937cae68957b65e9c7dcd555056651ec2ceb0e31
dcruzbosco <dcruzbosco@gmail.com>
2012-07-17 12:46:48 +0530

    setter methods added

--- GitTestProject/src/HelloWorld.java
+++ GitTestProject/src/HelloWorld.java
@@ -4,4 +4,12 @@ public class HelloWorld {
 	private String name;
 	private String address;
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
